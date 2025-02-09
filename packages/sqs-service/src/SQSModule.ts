71f68bc9367f351bf94c5abcc94b53338f2ebbbe
mdcoon <mdcoon1@yahoo.com>
2025-02-08 19:23:31 -0500

    Updated infra sqs service to use actual sqs if in production

--- packages/sqs-service/src/SQSModule.ts
+++ packages/sqs-service/src/SQSModule.ts
@@ -1,19 +1,26 @@
-import { Module } from "@nestjs/common";
+import { DynamicModule, Module } from "@nestjs/common";
 import { SebuConfigModule } from "@sebu/config";
 import { ASQSService } from "./ASQSService";
-//import { SQSService } from "./SQSService";
 import { SQSPostingService } from "./SQSPostingService";
+import { SQSService } from "./SQSService";
 
-const providers = [
-    {
-        provide: ASQSService,
-        //useClass: SQSService
-        useClass: SQSPostingService
-    }
-];
 @Module({
-    imports: [SebuConfigModule],
-    providers,
-    exports: providers
 })
-export class SQSModule{};
+export class SQSModule{
+
+    static forRoot(): DynamicModule {
+        const isDev = process.env.NODE_ENV === "development";
+        const providers = [
+            {
+                provide: ASQSService,
+                useClass: isDev ? SQSPostingService : SQSService
+            }
+        ];
+        return {
+            module: SQSModule,
+            imports: [SebuConfigModule],
+            providers,
+            exports: providers
+        }
+    }
+};
